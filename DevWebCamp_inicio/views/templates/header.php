3009889a1a1f6db65e4808a47c55f185416b3f0f
LeonardoJFD <leonardojosefonsecadono@gmail.com>
2024-04-23 14:41:54 -0400

    Mi segundo proyecto en github

---
+++ DevWebCamp_inicio/views/templates/header.php
@@ -0,0 +1,37 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/registro" class="header__enlace">Iniciar Sesión</a>
+        </nav>
+
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">
+                    &#60;DevWebCamp />
+                </h1>
+            </a>
+
+            <p class="header__texto">Dicembre 5-01 - 2024</p>
+            <p class="header__texto">En Linea - Presencial</p>
+
+            <a href="/registro" class="header__boton">Pase de video conferencia</a>
+        </div>
+    </div>
+</header>
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/">
+            <h2 class="barra__logo">
+                &#60;DevWebCamp />
+            </h2>
+             
+        </a>
+        <nav class="navegacion">
+        <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+        <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+        <a href="/workshops-conferencias" class="navegacion__enlace">Workshop / Conferencias</a>
+        <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
